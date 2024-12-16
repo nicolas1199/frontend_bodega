cc0dcd9f1f205510c79b9373a295f29bad5c878d
Nico <nicolas.opazo2001@alumnos.ubiobio.cl>
2024-12-16 16:35:50 -0300

    Add dotenv and dotenv-webpack for environment variable management; update API base URL and Webpack port configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import { fileURLToPath } from 'url'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+import Dotenv from 'dotenv-webpack'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -33,6 +34,7 @@ export default {
         new HtmlWebpackPlugin({
             template: './src/index.html',
         }),
+        new Dotenv(),
     ],
     resolve: {
         extensions: ['.js', '.jsx'],
@@ -41,7 +43,7 @@ export default {
         static: {
             directory: path.join(__dirname, 'dist'),
         },
-        port: import.meta.env.PORT || 3001,
+        port: process.env.PORT || 3001,
         open: true,
         historyApiFallback: true,
     },
